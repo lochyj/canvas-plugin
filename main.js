a38d3fa2c80f00f10afc9f4754155ca8391e4a3e
Lachlan Jowett <lochyjowett1@gmail.com>
2023-02-10 19:44:47 +1100

    quick push

--- main.js
+++ main.js
@@ -1,1 +1,70 @@
+// I am sorry for this terrible affront to programmers, and i am sorry that you had to look at this... But it works and I don't want to change it.
 console.log("Canvas plugin loaded successfully");
+
+
+if (window.location.href.split("/")[3] == "courses") classesFormat();
+
+function classesFormat() {
+    var course_syllabus = document.getElementById("course_syllabus");
+    var quick_access_links = course_syllabus.getElementsByTagName("table")[0].getElementsByTagName("tbody")[0];
+    var table_rows = quick_access_links.getElementsByTagName("tr");
+
+    var links = [];
+    var text = [];
+
+    for (let i = 0; i < table_rows.length; i++) {
+
+        for (let j = 0; j < table_rows[i].getElementsByTagName("td").length; j++) {
+
+            if (table_rows[i].getElementsByTagName("td")[j].children[0].tagName == "H3") {
+
+                if (table_rows[i].getElementsByTagName("td")[j].getElementsByTagName("h3")[0].children[0].tagName == "A") {
+
+                    var temp = table_rows[i].getElementsByTagName("td")[j].getElementsByTagName("h3")[0].getElementsByTagName("a")[0];
+
+                    links.push(temp.href);
+                    text.push(temp.innerText);
+
+                }
+
+            }
+
+        }
+
+    }
+
+    course_syllabus.removeChild(course_syllabus.getElementsByTagName("table")[0]);
+
+    course_syllabus.removeChild(course_syllabus.getElementsByTagName("h2")[2]);
+    course_syllabus.removeChild(course_syllabus.getElementsByTagName("p")[3]);
+
+    // Create a div element with a series of divs inside that that contain the links with the text of the text array
+    if (links.length != 0) {
+        var LinksContainer = document.createElement("div");
+        LinksContainer.className = "links-container";
+
+        var LinksTitle = document.createElement("h2");
+        LinksTitle.style.textAlign = "left"
+        LinksTitle.innerText = "Quick Links:";
+        LinksTitle.style.fontWeight = "bolder";
+
+        LinksContainer.appendChild(LinksTitle);
+
+        for (let i = 0; i < text.length; i++) {
+            if (text[i] == "") {
+                continue;
+            }
+            var container = document.createElement("div");
+            var linkEl = document.createElement("a");
+
+            container.appendChild(linkEl);
+
+            linkEl.innerText = text[i];
+            linkEl.href = links[i];
+            LinksContainer.appendChild(container);
+        }
+
+        course_syllabus.appendChild(LinksContainer);
+    }
+}
+
